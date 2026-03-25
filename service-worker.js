285d59eb39d7d310adfb37b4d21ad15178bcb68b
SashkoIvanov <aleks.iv672@gmail.com>
2026-03-25 22:24:39 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'streaktodo-v1';
 const ASSETS = [
   '/',
-  '/index.html',
-  '/app.css',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'index.html',
+  'app.css',
+  'app.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
